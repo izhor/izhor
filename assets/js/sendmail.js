5a88b2d1088d9c2c0b5198c43725753cc839699b
Izoru <alif.zhorif@gmail.com>
2022-02-04 00:59:12 +0700

    maintenance for email form + new page (project)

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,1 +1,23 @@
-function sendEmail(){var sender=document.getElementById('name').value;var email=document.getElementById('email').value;var question=document.getElementById('question').value;var content=sender+" ("+email+"): "+question;if(sender==""||email==""||question==""){alert(" message failed : all text input must be filled")}else{Email.send({SecureToken:"44426cf6-395b-4ae9-b240-df3794768ce4",To:'alif.zhorif@gmail.com',From:"izoru.experimental@gmail.com",Subject:"Question",Body:content}).then(message=>alert("Message Sent Successfully"));}} function emptyInput(){document.getElementById('name').value='';document.getElementById('email').value='';document.getElementById('question').value='';}
+function sendEmail(){var sender=document.getElementById('name').value;
+var email=document.getElementById('email').value;
+var question=document.getElementById('question').value;
+var content=sender+" ("+email+"): "+question;
+if(sender==""||email==""||question=="")
+    {
+    alert(" message failed : all text input must be filled")
+    }else{
+    Email.send(
+        {
+            SecureToken:"5ed1c3a9-6727-46ba-849b-865ef135f9eb",
+            To:'alif.zhorif@gmail.com',
+            From:"izoru.experimental@gmail.com",
+            Subject:"Question from "+sender,
+            Body:content
+        }).then(message=>alert(message));
+    }
+} 
+function emptyInput(){
+    document.getElementById('name').value='';
+    document.getElementById('email').value='';
+    document.getElementById('question').value='';
+}
